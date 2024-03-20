720607e0562962c98082e4c2f4a6afba1dbc76a6
tubone <tubo.yyyuuu@gmail.com>
2024-03-20 14:31:22 +0900

    add pwa

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 import { defineConfig } from 'astro/config';
+import serviceWorker from "astrojs-service-worker";
 import partytown from "@astrojs/partytown";
+import webmanifest from 'astro-webmanifest';
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,6 +11,17 @@ export default defineConfig({
             config: {
                 forward: ["dataLayer.push"],
             },
+        }),
+        process.env.NODE_ENV === 'production' ? serviceWorker() : null,
+        webmanifest({
+            name: 'Yu Otsubo - Business Card Site',
+            icon: 'public/icon.png',
+            short_name: 'Yu Otsubo',
+            description: 'Yu Otsubo - Business Card Site',
+            start_url: 'https://tubone-project24.onrender.com/#testtest',
+            theme_color: '#4bffde',
+            background_color: '#4bffde',
+            display: 'standalone',
         }),
     ],
 });
